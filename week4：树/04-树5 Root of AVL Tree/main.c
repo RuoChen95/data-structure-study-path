2d0a6d8288e990db94531d738585c2959f6ed121
RuoChen95 <xrc_1995@163.com>
2019-07-03 22:14:56 +0800

    10：Root of AVL Tree 提交

--- week4：树/04-树5 Root of AVL Tree/main.c
+++ week4：树/04-树5 Root of AVL Tree/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // malloc
 
-typedef struct AVLNode *AVLTree;
-struct AVLNode {
-    int Data;
-    int Height;
-    AVLTree Left;
-    AVLTree Right;
+typedef struct AVLNode *Position;
+typedef Position AVLTree; /* AVL树类型 */
+typedef int ElementType;
+struct AVLNode{
+    ElementType Data; /* 结点数据 */
+    AVLTree Left;     /* 指向左子树 */
+    AVLTree Right;    /* 指向右子树 */
+    int Height;       /* 树高 */
 };
-AVLTree Insert(int X, AVLTree T);
-int GetHeight(AVLTree T);
+
+AVLTree Insert( AVLTree T, ElementType X );
+AVLTree SingleLeftRotation ( AVLTree A );
+AVLTree DoubleLeftRightRotation ( AVLTree A );
+AVLTree SingleRightRotation ( AVLTree A );
+AVLTree DoubleRightLeftRotation ( AVLTree A );
+int Max ( int a, int b );
+int GetHeight( AVLTree A );
+
 int main(void) {
     AVLTree T = NULL;
     int n;
@@ -17,7 +26,7 @@ int main(void) {
     while(n--) {
         int x;
         scanf("%d", &x);
-        T = Insert(x, T);
+        T = Insert(T, x);
     }
 
     if (T) {
@@ -25,30 +34,98 @@ int main(void) {
     }
     return 0;
 }
-AVLTree Insert(int X, AVLTree T) {
-    if (T == NULL) {
+
+AVLTree Insert( AVLTree T, ElementType X )
+{ /* 将X插入AVL树T中，并且返回调整后的AVL树 */
+    if ( !T ) { /* 若插入空树，则新建包含一个结点的树 */
         T = (AVLTree)malloc(sizeof(struct AVLNode));
         T->Data = X;
         T->Height = 0;
-        T->Left = NULL;
-        T->Right = NULL;
-    } else if (X < T->Data) {
-        // 将X作为左值导入
-        T->Left = Insert(X, T->Left);
-        if (GetHeight(T->Left) - GetHeight(T->Right) == 2) {
-            if (X < T->Left->Data) {
-                T = SingleLeft(T);
-            } else {
-                T = DoubleLeft(T);
-            }
-        }
-    } else if (X > T->Data) {
+        T->Left = T->Right = NULL;
+    } /* if (插入空树) 结束 */
 
-    }
-    // height
+    else if ( X < T->Data ) {
+        /* 插入T的左子树 */
+        T->Left = Insert( T->Left, X);
+        /* 如果需要左旋 */
+        if ( GetHeight(T->Left)-GetHeight(T->Right) == 2 )
+            if ( X < T->Left->Data )
+                T = SingleLeftRotation(T);      /* 左单旋 */
+            else
+                T = DoubleLeftRightRotation(T); /* 左-右双旋 */
+    } /* else if (插入左子树) 结束 */
+
+    else if ( X > T->Data ) {
+        /* 插入T的右子树 */
+        T->Right = Insert( T->Right, X );
+        /* 如果需要右旋 */
+        if ( GetHeight(T->Left)-GetHeight(T->Right) == -2 )
+            if ( X > T->Right->Data )
+                T = SingleRightRotation(T);     /* 右单旋 */
+            else
+                T = DoubleRightLeftRotation(T); /* 右-左双旋 */
+    } /* else if (插入右子树) 结束 */
+
+    /* else X == T->Data，无须插入 */
+
+    /* 别忘了更新树高 */
+    T->Height = Max( GetHeight(T->Left), GetHeight(T->Right) ) + 1;
 
     return T;
 }
-int GetHeight(AVLTree T) {
 
+AVLTree SingleLeftRotation ( AVLTree A )
+{ /* 注意：A必须有一个左子结点B */
+    /* 将A与B做左单旋，更新A与B的高度，返回新的根结点B */
+
+    AVLTree B = A->Left;
+    A->Left = B->Right;
+    B->Right = A;
+    A->Height = Max( GetHeight(A->Left), GetHeight(A->Right) ) + 1;
+    B->Height = Max( GetHeight(B->Left), A->Height ) + 1;
+
+    return B;
+}
+
+AVLTree DoubleLeftRightRotation ( AVLTree A )
+{ /* 注意：A必须有一个左子结点B，且B必须有一个右子结点C */
+    /* 将A、B与C做两次单旋，返回新的根结点C */
+
+    /* 将B与C做右单旋，C被返回 */
+    A->Left = SingleRightRotation(A->Left);
+    /* 将A与C做左单旋，C被返回 */
+    return SingleLeftRotation(A);
+}
+
+/*************************************/
+/* 对称的右单旋与右-左双旋请自己实现 */
+/*************************************/
+
+AVLTree SingleRightRotation ( AVLTree A ) {
+    AVLTree B = A->Right;
+    A->Right = B->Left;
+    B->Left = A;
+    A->Height = Max( GetHeight(A->Right), GetHeight(A->Left) ) + 1;
+    B->Height = Max( GetHeight(B->Right), A->Height ) + 1;
+
+    return B;
+}
+AVLTree DoubleRightLeftRotation ( AVLTree A ) {
+    A->Right = SingleLeftRotation(A->Right);
+    /* 将A与C做左单旋，C被返回 */
+    return SingleRightRotation(A);
+
+}
+
+int Max ( int a, int b )
+{
+    return a > b ? a : b;
+}
+
+int GetHeight( AVLTree A ) {
+    if (!A) {
+        return -1;
+    } else {
+        return A->Height;
+    }
 }
