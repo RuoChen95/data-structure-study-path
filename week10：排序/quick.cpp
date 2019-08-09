6d4052cf2555ce9917ce760b8ce522de045b260f
RuoChen95 <xrc_1995@163.com>
2019-08-09 19:49:37 +0800

    题目：快速排序

--- week10：排序/quick.cpp
+++ week10：排序/quick.cpp
@@ -13,9 +13,10 @@ void getArray (ElementType A[], int N);
 void showArray (ElementType A[], int N);
 void Swap (int * A, int * B);
 
-void quick_sort(ElementType A[], int N);
-void Quicksort(ElementType A[], int Left, int Right);
-ElementType Median3(ElementType A[], int Left, int Right);
+ElementType Median3( ElementType A[], int Left, int Right );
+void Qsort( ElementType A[], int Left, int Right );
+void QuickSort( ElementType A[], int N );
+void InsertionSort( ElementType A[], int Left, int Right );
 int main(void) {
     int N;
     int A[MaxNumber] = {0};
@@ -23,7 +24,7 @@ int main(void) {
 
     getArray(A, N);
 
-    quick_sort(A, N);
+    QuickSort(A, N);
 
     showArray(A, N);
     return 0;
@@ -41,55 +42,65 @@ void showArray (ElementType A[], int N) {
     for (i = 1; i < N; i++) {
         printf(" %d", A[i]);
     }
+    printf("\n");
 }
-void Swap (int * A, int * B) {
-    int temp = *A;
-    *A = *B;
-    *B = temp;
-}
-
-
-void quick_sort(ElementType A[], int N) {
-    Quicksort(A, 0, N - 1);
+void Swap (int * a, int * b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void Quicksort(ElementType A[], int Left, int Right) {
-    int Pivot = Median3(A, Left, Right);
-    int i = Left + 1;
-    int j = Right - 2;
 
-    while (i < j) {
-        while (A[i] < Pivot) {
-            i++;
+
+/* 快速排序 */
+
+ElementType Median3( ElementType A[], int Left, int Right )
+{
+    int Center = (Left+Right) / 2;
+    if ( A[Left] > A[Center] )
+        Swap( &A[Left], &A[Center] );
+    if ( A[Left] > A[Right] )
+        Swap( &A[Left], &A[Right] );
+    if ( A[Center] > A[Right] )
+        Swap( &A[Center], &A[Right] );
+    /* 此时A[Left] <= A[Center] <= A[Right] */
+    Swap( &A[Center], &A[Right-1] ); /* 将基准Pivot藏到右边*/
+    /* 只需要考虑A[Left+1] … A[Right-2] */
+    return  A[Right-1];  /* 返回基准Pivot */
+}
+
+void Qsort( ElementType A[], int Left, int Right )
+{ /* 核心递归函数 */
+    int Pivot, Cutoff = 50, Low, High;
+
+    if ( Cutoff <= Right-Left ) { /* 如果序列元素充分多，进入快排 */
+        Pivot = Median3( A, Left, Right ); /* 选基准 */
+        Low = Left; High = Right-1;
+        while (1) { /*将序列中比基准小的移到基准左边，大的移到右边*/
+            while ( A[++Low] < Pivot ) ;
+            while ( A[--High] > Pivot ) ;
+            if ( Low < High ) Swap( &A[Low], &A[High] );
+            else break;
         }
-        while (A[j] > Pivot) {
-            j--;
-        }
-        if (i < j) {
-            Swap(&A[i], &A[j]);
-        } else {
-            break;
-        }
+        Swap( &A[Low], &A[Right-1] );   /* 将基准换到正确的位置 */
+        Qsort( A, Left, Low-1 );    /* 递归解决左边 */
+        Qsort( A, Low+1, Right );   /* 递归解决右边 */
     }
-    Swap(&A[i], &A[Right-1]);
-
-    Quicksort(A, Left, i-1);
-    Quicksort(A, i+1, Right);
+    else InsertionSort( A, Left, Right ); /* 元素太少，用简单排序 */
 }
 
-ElementType Median3(ElementType A[], int Left, int Right) {
-    int Center = (Left + Right)/2;
-    if (A[Left] > A[Center]) {
-        Swap(&A[Left], &A[Center]);
-    }
-    if (A[Right] < A[Left]) {
-        Swap(&A[Right], &A[Left]);
-    }
-    if (A[Center] > A[Right]) {
-        Swap(&A[Center], &A[Right]);
-    }
+void QuickSort( ElementType A[], int N )
+{ /* 统一接口 */
+    Qsort( A, 0, N-1 );
+}
 
-    //return A[Center];
-    Swap(&A[Center], &A[Right-1]);
-    return A[Right-1];
+void InsertionSort( ElementType A[], int Left, int Right ) {
+    int p, temp, i;
+    for (p = Left+1; p <= Right; p++) {
+        temp = A[p];
+        for (i = p; i >= Left+1 && temp < A[i - 1]; i--) {
+            A[i] = A[i -1];
+        }
+        A[i]= temp;
+    }
 }
