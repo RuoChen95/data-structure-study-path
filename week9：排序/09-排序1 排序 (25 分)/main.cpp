63411ae06bb9d5ae9bfcc796c7f9b1d48feeb5dc
RuoChen95 <xrc_1995@163.com>
2019-08-09 15:15:02 +0800

    复习：简单排序，希尔排序，堆排序

--- week9：排序/09-排序1 排序 (25 分)/main.cpp
+++ week9：排序/09-排序1 排序 (25 分)/main.cpp
@@ -21,6 +21,8 @@ void Merge_Sort(ElementType A[], int N);
 void Switch (int * a, int * b);
 void MSort( ElementType A[], ElementType TmpA[], int L, int RightEnd);
 void Merge (ElementType A[], ElementType TmpA[], int L, int R, int RightEnd);
+
+void PercDown( ElementType A[], int p, int N );
 int main(void) {
     int N;
     int A[MaxNumber] = {0};
@@ -47,21 +49,25 @@ void showArray (ElementType A[], int N) {
         printf(" %d", A[i]);
     }
 }
-
 void Switch (int * a, int * b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
+
+
+
+
 void bubble_Sort( ElementType A[], int N) {
-    int i;
-    int p;
+    int i = 0;
+    int p = N - 1;
     int flag;
-    for (p = N - 1; p > 0; p--) {
+
+    for (p = N - 1; p >=0; p--) {
         flag = 0;
         for (i = 0; i < p; i++) {
-            if (A[i] > A[i+1]) {
+            if (A[i] > A[i + 1]) {
                 Switch(&A[i], &A[i+1]);
                 flag = 1;
             }
@@ -71,95 +77,81 @@ void bubble_Sort( ElementType A[], int N) {
         }
     }
 }
-
+// index为0默认已经在手上了
 void insert_Sort( ElementType A[], int N) {
-    int P, Tmp, i;
-    for (P = 1; P < N; P++) {
-        Tmp = A[P];
-        for (i = P; i >= 1 && A[i-1] > Tmp; i--) {
-            A[i] = A[i-1];
+    int p, temp, i;
+    for (p = 1; p < N; p++) {
+        temp = A[p];
+        for (i = p; i >= 1 && temp < A[i - 1]; i--) {
+            A[i] = A[i -1];
         }
-        A[i] = Tmp;
+        A[i]= temp;
     }
 }
+
+
+
+
 
 void shell_Sort( ElementType A[], int N) {
-    int P, Tmp, i, D;
-    for ( D = N/2; D > 0; D = D/2) {
-        for (P = D; P < N; P++) {
-            Tmp = A[P];
-            for (i = P; i >= D && A[i-D] > Tmp; i = i-D) {
-                A[i] = A[i-D];
+    int D;
+    int p, temp, i;
+    for (D = N/2; D >= 1; D = D/2) { // 原始的增量序列
+        for (p = D; p < N; p++) { // 插入排序
+            temp = A[p];
+            for (i = p; i >= D && temp < A[i - D]; i = i - D) {
+                A[i] = A[i -D];
             }
-            A[i] = Tmp;
+            A[i]= temp;
         }
     }
 }
-
-
 
 
 
 
 
 void Heap_Sort_Simple( ElementType A[], int N) {
-    // 用到了c++的标准模板库（优先队列 - 堆)
-    priority_queue<ElementType, vector<ElementType>, greater<ElementType>> q;
+    priority_queue <ElementType, vector<ElementType>, greater<ElementType>> q;
     int i;
-    ElementType TmpA[MaxNumber];
+    ElementType temp[MaxNumber];
     for (i = 0; i < N; i++) {
         q.push(A[i]);
     }
-
     for (i = 0; i < N; i++) {
-        TmpA[i] = q.top();
+        temp[i]= q.top();
         q.pop();
     }
-
-    for (i = 0; i < N; i++) {
-        A[i] = TmpA[i];
+    for (i = 0 ; i < N; i++) {
+        A[i] = temp[i];
     }
 }
-
 void Heap_Sort( ElementType A[], int N )
 {
     int i;
-
-    // 建立最大堆
-    for ( i=N/2-1; i>=0; i-- )
+    for ( i=N/2; i>=0; i-- ) {
         PercDown( A, i, N );
+    }
 
     for ( i=N-1; i>0; i-- ) {
-        // 删除最大堆顶
         Switch( &A[0], &A[i] );
-
-        // 重新建立最大堆1
         PercDown( A, 0, i );
     }
 }
 void PercDown( ElementType A[], int p, int N )
 {
-    /* 改编代码4.24的PercDown( MaxHeap H, int p )    */
-    /* 将N个元素的数组中以A[p]为根的子堆调整为最大堆 */
-    int Parent, Child;
-    ElementType X;
-
-    X = A[p]; /* 取出根结点存放的值 */
-    for( Parent=p; (Parent*2+1)<N; Parent=Child ) {
-        Child = Parent * 2 + 1;
-        if( (Child!=N-1) && (A[Child]<A[Child+1]) )
-            Child++;  /* Child指向左右子结点的较大者 */
-        if( X >= A[Child] ) break; /* 找到了合适位置 */
-        else  /* 下滤X */
-            A[Parent] = A[Child];
+    int parent, child;
+    ElementType X = A[p];
+    for (parent = p; (parent*2+1)<N; parent = child) {
+        child = parent * 2 + 1;
+        if ((A[child] < A[child+1]) && (child != N - 1)) {
+            child++;
+        }
+        if (X >= A[child]) break;
+        else A[parent] = A[child];
     }
-    A[Child] = X;
+    A[parent] = X;
 }
-
-
-
-
-
 
 
 // 标准的函数接口
