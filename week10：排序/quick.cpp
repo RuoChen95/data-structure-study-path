0365502a134b6e0d5d57ae3526bb345bb8b3ba73
RuoChen95 <xrc_1995@163.com>
2019-08-12 13:43:59 +0800

    题目：快速排序的pivot以及cutoff的比较

--- week10：排序/quick.cpp
+++ week10：排序/quick.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <queue>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <time.h> 
 
 using namespace std;
 
@@ -14,6 +14,7 @@ void showArray (ElementType A[], int N);
 void Swap (int * A, int * B);
 
 ElementType Median3( ElementType A[], int Left, int Right );
+ElementType First( ElementType A[], int Left, int Right);
 void Qsort( ElementType A[], int Left, int Right );
 void QuickSort( ElementType A[], int N );
 void InsertionSort( ElementType A[], int Left, int Right );
@@ -52,45 +53,99 @@ void Swap (int * a, int * b) {
 
 
 
-/* 快速排序 */
-
 ElementType Median3( ElementType A[], int Left, int Right )
 {
-    int Center = (Left+Right) / 2;
-    if ( A[Left] > A[Center] )
-        Swap( &A[Left], &A[Center] );
-    if ( A[Left] > A[Right] )
-        Swap( &A[Left], &A[Right] );
-    if ( A[Center] > A[Right] )
-        Swap( &A[Center], &A[Right] );
-    /* 此时A[Left] <= A[Center] <= A[Right] */
-    Swap( &A[Center], &A[Right-1] ); /* 将基准Pivot藏到右边*/
-    /* 只需要考虑A[Left+1] … A[Right-2] */
-    return  A[Right-1];  /* 返回基准Pivot */
+    int Center = (Left + Right)/2;
+    if (A[Left] > A[Center]){
+        Swap(&A[Left], &A[Center]);
+    }
+    if (A[Left] > A[Right]) {
+        Swap(&A[Left], &A[Right]);
+    }
+
+    if (A[Center] > A[Right]) {
+        Swap(&A[Center], &A[Right]);
+    }
+
+    Swap(&A[Center], &A[Right-1]);
+
+    return A[Right-1];
+}
+
+ElementType First( ElementType A[], int Left, int Right)
+{
+	int Center = Left + 1;
+    if (A[Left] > A[Center]){
+        Swap(&A[Left], &A[Center]);
+    }
+    if (A[Left] > A[Right]) {
+        Swap(&A[Left], &A[Right]);
+    }
+
+    if (A[Center] > A[Right]) {
+        Swap(&A[Center], &A[Right]);
+    }
+
+    Swap(&A[Center], &A[Right-1]);
+
+    return A[Right-1];
+}
+
+ElementType Random( ElementType A[], int Left, int Right)
+{
+	srand(time(NULL));
+	int Center = (rand() % 
+           (Right - Left - 1)) + Left + 1;
+//    printf("----%d %d %d----\n", Center, Left, Right);
+    if (A[Left] > A[Center]){
+        Swap(&A[Left], &A[Center]);
+    }
+    if (A[Left] > A[Right]) {
+        Swap(&A[Left], &A[Right]);
+    }
+
+    if (A[Center] > A[Right]) {
+        Swap(&A[Center], &A[Right]);
+    }
+
+    Swap(&A[Center], &A[Right-1]);
+
+    return A[Right-1];
 }
 
 void Qsort( ElementType A[], int Left, int Right )
-{ /* 核心递归函数 */
-    int Pivot, Cutoff = 50, Low, High;
+{
+    int P, Cutoff = 50, Low, High;
 
-    if ( Cutoff <= Right-Left ) { /* 如果序列元素充分多，进入快排 */
-        Pivot = Median3( A, Left, Right ); /* 选基准 */
-        Low = Left; High = Right-1;
-        while (1) { /*将序列中比基准小的移到基准左边，大的移到右边*/
-            while ( A[++Low] < Pivot ) ;
-            while ( A[--High] > Pivot ) ;
-            if ( Low < High ) Swap( &A[Low], &A[High] );
-            else break;
+    if ( Cutoff <= Right-Left ) {
+//		// Median 
+//		P = Median3(A, Left, Right);
+//		// First
+//		P = First(A, Left, Right);
+		// Random
+		P = Random(A, Left, Right);
+		
+        Low = Left;
+        High = Right - 1;
+        
+        while(1) {
+            while(P > A[++Low]){}
+            while(P < A[--High]){}
+            if (Low < High) {
+                Swap(&A[Low], &A[High]);
+            } else {
+                break;
+            }
         }
-        Swap( &A[Low], &A[Right-1] );   /* 将基准换到正确的位置 */
-        Qsort( A, Left, Low-1 );    /* 递归解决左边 */
-        Qsort( A, Low+1, Right );   /* 递归解决右边 */
+        Swap(&A[Low], &A[Right-1]);
+        Qsort(A, Left, Low-1);
+        Qsort(A, Low+1, Right);
     }
-    else InsertionSort( A, Left, Right ); /* 元素太少，用简单排序 */
+    else InsertionSort( A, Left, Right );
 }
 
 void QuickSort( ElementType A[], int N )
-{ /* 统一接口 */
+{
     Qsort( A, 0, N-1 );
 }
 
