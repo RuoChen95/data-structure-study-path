779767b7ae10f261908db7900c836c592f20ce84
RuoChen95 <xrc_1995@163.com>
2019-08-25 12:25:36 +0800

    完成：01-复杂度2

--- week1：基本概念/01-复杂度2-Maximum Subsequence Sum/main.c
+++ week1：基本概念/01-复杂度2-Maximum Subsequence Sum/main.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 
-int MaxSum ( int a[], int n) {
-    int thisSum = 0;
-    int maxSum = 0;
-    int startIndexFack = 0;
-    int startIndexReal = 0;
-    int endIndex = 0;
-    for (int i = 0; i < n; i++) {
-        thisSum += a[i];
+#define MAXNUMBER 10000
 
-        if (thisSum > maxSum) {
-            maxSum = thisSum;
-            endIndex = i;
-            if (startIndexFack <= endIndex) {
-                startIndexReal = startIndexFack;
-            }
-        } else if (thisSum <= 0) {
-            thisSum = 0;
-            startIndexFack = i+1;
-        }
-    }
-    printf("%d %d %d\n", maxSum, startIndexReal,endIndex);
-    return 0;
-}
+void MaxSum ( int a[], int N);
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    int length = 0;
-    scanf("%d", &length);
+    int N = 0;
+    scanf("%d", &N);
 
-    int A[length];
-    for (int i=0; i<length; i++) {
+    int A[MAXNUMBER];
+    for (int i=0; i<N; i++) {
         scanf("%d", &A[i]);
     }
 
-    MaxSum(A, length);
+    MaxSum(A, N);
 
     return 0;
+}
+
+void MaxSum ( int a[], int N) {
+	int i;
+	int ThisSum, MaxSum;
+	int m, n, temp;
+	ThisSum = 0;
+	MaxSum = -1; // 为了正确判断出0的存在区域 
+	m = 0;
+	n = 0;
+	temp = 0;
+	for (i = 0; i < N; i++){
+		ThisSum += a[i];
+		if (ThisSum > MaxSum) {
+			MaxSum = ThisSum;
+			n = i;
+			m = temp;
+		} 
+		if (ThisSum < 0) {
+			ThisSum = 0;
+			temp = i+1;
+		}
+	}
+
+	if (MaxSum >= 0) {
+		printf("%d %d %d", MaxSum, a[m], a[n]);
+	} else {
+		printf("%d %d %d", 0, a[0], a[N-1]);
+	}
 }
